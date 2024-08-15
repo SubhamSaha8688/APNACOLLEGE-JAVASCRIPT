7e8de54617567eb7f275d438a6efbc5543419da6
SubhamSaha8688 <shadowmonarch8688@gmail.com>
2024-08-15 16:43:55 +0530

    first commit

---
+++ LECTURE1/Practice2.js
@@ -0,0 +1,9 @@
+const profile={
+    username : "@subhamsaha",
+    isfollow : false,
+    followers : 1000,
+    following : 500,
+    
+};
+
+console.log(profile);
---
+++ LECTURE1/first.js
@@ -0,0 +1,42 @@
+fullname="SubhamSaha"
+x=null;
+y=undefined;
+console.log("Subham Saha");
+console.log("i Love JavaScript");
+console.log(fullname);
+isfollow=false;
+console.log(isfollow);
+
+var a = "saha"
+let b = "saha"
+b=15
+const myname = "SubhamSaha"
+console.log(a,b,myname);
+{
+    let c=10;
+    console.log(c);
+}
+{
+    let c=10;
+    console.log(c);
+}
+
+console.log(typeof isfollow);
+
+const student ={
+    name: "SubhamSaha",
+    age: 25,
+    address: {
+        street: "21 Main St",
+        city: "New York",
+        state: "NY"
+    }
+    
+}
+
+console.log(typeof student);
+console.log(student["age"]);
+console.log(student.age);
+
+student.age=student.age+1;
+console.log(typeof student.address);
