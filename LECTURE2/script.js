e8b33463ef91696204ddade94bcef6037687e8aa
SubhamSaha8688 <shadowmonarch8688@gmail.com>
2024-08-15 16:54:28 +0530

    check

---
+++ LECTURE2/script.js
@@ -0,0 +1,18 @@
+//This code prints hello world
+console.log("hello world");
+
+//Arithmetic operations
+
+let a = 5;
+let b = 2;
+
+console.log("a: ", a, " & b: ",b);
+console.log("a + b =",a+b);
+
+console.log("a - b =",a-b);
+
+console.log("a * b =",a*b);
+
+console.log("a / b =",a/b);
+
+console.log("a % b =",a%b);
